888541506e054e36a3ebd74bad90803fb11df815
hzc1998 <2323168280@qq.com>
2022-02-09 16:22:26 +0800

    refactor(i386): rebase main.c

--- src/platform/i386/hal/main.c
+++ src/platform/i386/hal/main.c
@@ -14,18 +14,11 @@
 
 #define NX_LOG_NAME "HAL_PlatformMain"
 #include <utils/log.h>
-#include <sched/process.h>
 
 NX_INTERFACE void HAL_PlatformMain(void)
 {
     NX_LOG_I("PC32 platform main running...\n");
 
-    int i;
-    for (i = 0; i < 256; i++)
-    {
-        NX_Error err = NX_ProcessExecute("nxbase_test", "nxbase_test.bin", 0);
-        NX_LOG_I("CREATE: %d", err);
-    }
 }
 
 NX_PUBLIC void HAL_DirectUartGetcHandler(char data)
